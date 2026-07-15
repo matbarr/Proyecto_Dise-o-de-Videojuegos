6201f7aa6a61203633a2fea951c9d04913b47247
Esteban <jorgeortegamarenco@gmail.com>
2026-07-15 02:22:34 -0600

    ultm

--- ArcaneTheLastSpell/Assets/Scripts/PlayerMovement.cs
+++ ArcaneTheLastSpell/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movimiento")]
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float jumpForce = 11f;
+
+    [Header("Detección del suelo")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private float horizontalInput;
+    private bool jumpRequested;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpRequested = true;
+        }
+
+        if (horizontalInput > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (horizontalInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        if (jumpRequested && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        jumpRequested = false;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
